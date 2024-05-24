b41d0bc048d29d99a4ca1d7250a1b224cc431ce0
FireLord <firelord.xda@gmail.com>
2024-05-24 16:33:17 +0530

    app: Add Firebase auth plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sample_news/injection_container.dart';
 import 'package:sample_news/presentation/screen/auth/LoginScreen.dart';
-import 'package:sample_news/presentation/screen/bottomTab/MainTabScreen.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await initDependency();
   runApp(const MyApp());
 }
